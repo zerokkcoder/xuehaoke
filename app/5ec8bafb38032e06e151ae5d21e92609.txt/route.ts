0f87ea9231091a7f3fb33d3811ee688875a4d443
zerokk <918031078@qq.com>
2025-12-12 10:30:38 +0800

    refactor: 将动态路由内容移至静态文件
    将原本通过动态路由生成的内容改为直接使用静态文本文件，简化实现并提高性能

--- app/5ec8bafb38032e06e151ae5d21e92609.txt/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export const dynamic = 'force-dynamic'
-
-export async function GET(req: Request) {
-  const body = '9b4a2ce3877e6e39e17dcd79a791ad0f3075008b'
-  return new NextResponse(body, { headers: { 'Content-Type': 'text/plain; charset=utf-8' } })
-}
